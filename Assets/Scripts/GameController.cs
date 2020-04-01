44eed11526926ddd24624718e11c580ef5501f5a
Mantarus <m.mantarus@gmail.com>
2020-04-02 01:53:16 +0300

    Add GameController class

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public GameObject carriagePrefab;
+    public List<GameObject> bricks;
+    
+    public int lives = 3;
+    
+    
+    
+}
