4edb4b00a7d13f982edc40b53d6bcdbaf4f1bb62
Mantarus <m.mantarus@gmail.com>
2020-04-02 21:42:50 +0300

    Add bonus spawn

---
+++ Assets/Scripts/BonusMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BonusMover : MonoBehaviour
+{
+    public float initialVelocity;
+    public float rotationSpeed;
+    
+    private Rigidbody _rb;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (LayerMask.NameToLayer("Level") == other.gameObject.layer)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.velocity = Vector3.down * initialVelocity;
+        _rb.angularVelocity = new Vector3(0, rotationSpeed, 0);
+    }
+}
--- Assets/Scripts/GeneralBrick.cs
+++ Assets/Scripts/GeneralBrick.cs
@@ -3,14 +3,22 @@
 public class GeneralBrick : MonoBehaviour
 {
     public int lives = 1;
+    public float bonusSpawnChance;
+    public GameObject bonusPrefab;
     public GameObject gameControllerObject;
 
     private GameController _gameController;
+    private GameObject _bonus;
 
     private void Start()
     {
         _gameController = gameControllerObject.GetComponent<GameController>();
         _gameController.IncreaseBrickCount();
+        if (Random.Range(0, 100) < bonusSpawnChance)
+        {
+            SpawnBonus();
+            _bonus.SetActive(false);
+        }
     }
 
     private void OnCollisionExit(Collision other)
@@ -24,14 +32,23 @@ public class GeneralBrick : MonoBehaviour
             if (lives <= 0)
             {
                 _gameController.DecreaseBrickCount();
+                if (_bonus != null)
+                {
+                    _bonus.SetActive(true);
+                }
                 DestroyBrick();
             }
         }
     }
 
+    private void SpawnBonus()
+    {
+        _bonus = Instantiate(bonusPrefab, transform.position, transform.rotation);
+    }
+
     private void DestroyBrick()
     {
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 
 }
